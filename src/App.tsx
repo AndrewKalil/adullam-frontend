0a9b85978a02c6b7a40295c8365df6bbedfdf693
andrewkalil <andrew_kalil@hotmail.com>
2026-06-15 00:17:26 -0400

    feat - wire ProductsPage route, add tailwind config, update dependencies (06-products-crud)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { ProtectedRoute } from "~components";
 import { AppRoute } from "~constants";
-import { AppShell, CategoriesPage, DashboardPage, LoginPage } from "~pages";
+import { AppShell, CategoriesPage, DashboardPage, LoginPage, ProductsPage } from "~pages";
 import { AuthProvider } from "~providers";
 
 export const App = () => (
@@ -15,7 +15,7 @@ export const App = () => (
           <Route element={<ProtectedRoute />}>
             <Route element={<AppShell />}>
               <Route path={AppRoute.Dashboard} element={<DashboardPage />} />
-              <Route path={AppRoute.Products} element={<div className="p-6">Products</div>} />
+              <Route path={AppRoute.Products} element={<ProductsPage />} />
               <Route path={AppRoute.Categories} element={<CategoriesPage />} />
               <Route path={AppRoute.Discounts} element={<div className="p-6">Discounts</div>} />
               <Route path={AppRoute.PromoCodes} element={<div className="p-6">Promo Codes</div>} />
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export * from "./AppShell";
 export * from "./CategoriesPage";
 export * from "./DashboardPage";
 export * from "./LoginPage";
+export * from "./ProductsPage";
